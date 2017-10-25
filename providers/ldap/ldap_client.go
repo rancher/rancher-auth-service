42eaf141b636cb527c6c203e244cf984a8d1f74e
rajashree <rajashree.28m@gmail.com>
2017-10-25 10:23:43 -0700

    Ignore case in isType()

--- providers/ldap/ldap_client.go
+++ providers/ldap/ldap_client.go
@@ -557,7 +557,7 @@ func isType(search []*ldap.EntryAttribute, varType string) bool {
 	for _, attrib := range search {
 		if attrib.Name == "objectClass" {
 			for _, val := range attrib.Values {
-				if val == varType {
+				if strings.EqualFold(val, varType) {
 					return true
 				}
 			}
